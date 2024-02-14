2d58946cef42ed23f6089b354e7d4275b39a1b97
Progames723 <93779034+Progames723@users.noreply.github.com>
2024-02-14 10:16:52 +0700

    added stellarity assets

---
+++ common/src/main/java/dev/progames723/stellarity/Registers.java
@@ -0,0 +1,51 @@
+package dev.progames723.stellarity;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.damagesource.DamageType;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+
+public class Registers {
+	public static MobEffect registerEffect(String modid, String name, MobEffect obj, @Nullable Logger logger) {
+		if (logger != null) {
+			logger.info("Registered effect: " + name);
+		}
+		return Registry.register(BuiltInRegistries.MOB_EFFECT, new ResourceLocation(modid, name), obj);
+	}
+
+	public static Item registerItem(String modid, String name, Item obj, @Nullable Logger logger) {
+		if (logger != null) {
+			logger.info("Registered item: " + name);
+		}
+		return Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(modid, name), obj);
+	}
+
+	public static Block registerBlock(String modid, String name, Block obj, @Nullable Logger logger){
+		if (logger != null) {
+			logger.info("Registered block: " + name);
+		}
+		return Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(modid, name), obj);
+	}
+
+	public static BlockItem registerBlockItem(String modid, String name, BlockItem obj, @Nullable Logger logger){
+		if (logger != null) {
+			logger.info("Registered block item: " + name);
+		}
+		return Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(modid, name), obj);
+	}
+
+	public static ResourceKey<DamageType> registerDamageType(String modid, String name, @Nullable Logger logger) {
+		if (logger != null){
+			logger.info("Registered damage type: " + name);
+		}
+		return ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(modid, name));
+	}
+}
