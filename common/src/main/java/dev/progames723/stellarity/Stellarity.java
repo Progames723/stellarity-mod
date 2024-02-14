8a1149250e5d10b5ec28f721083b8074e5c04354
Progames723 <93779034+Progames723@users.noreply.github.com>
2024-02-14 09:53:24 +0700

    minor change

--- common/src/main/java/dev/progames723/stellarity/Stellarity.java
+++ common/src/main/java/dev/progames723/stellarity/Stellarity.java
@@ -3,8 +3,7 @@ package dev.progames723.stellarity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Stellarity
-{
+public class Stellarity {
 	public static final String MOD_ID = "stellarity";
 	public static final Logger LOGGER = LoggerFactory.getLogger("Stellarity");
 
